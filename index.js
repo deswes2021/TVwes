0135d22239023c4f95e009ce1a213e3918f254b7
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2025-08-14 17:52:41 -0600

    save

--- index.js
+++ index.js
@@ -166,9 +166,9 @@ function opKNL() {
     var nh = getHH();
     console.log(d1);
     if (/\.(mp4|m3u8|ytb)/i.test(d1)) {
-        if (!/(menu)/i.test(ito)) { location.replace('./index.html?' + nh + '#' + d1); }
+        location.replace('./index.html?' + nh + '#' + d1);
     } else if (/(\.html|\/activar)$/i.test(d1)) {
-        if (!/(menu)/i.test(ito)) { location.replace('./index.html?' + nh + '#' + d1); }
+        location.replace('./index.html?' + nh + '#' + d1);
     }
     return false;
 }
