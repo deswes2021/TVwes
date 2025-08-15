e86c455610f30195c5bc75682759fa1b1cc6df71
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2025-08-14 18:38:50 -0600

    SAVE INDEX

--- index.js
+++ index.js
@@ -24,23 +24,24 @@ function KScripts(scripts, callback) {
     cargarSiguiente();
 }
 
-
 /*--LISTA_SCRIPT-------------------*/
 function setINIC() {
-    ito = location.href.split('#')[1]||'menu';
-    if (/(menu)/i.test(ito)) {
-        KScripts(["https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js"], function () {setMENU();});
-    } else {
-        KScripts([
-            "https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js",
-            "https://cdn.jsdelivr.net/npm/clappr@0.3.13/dist/clappr.min.js",
-            "https://cdn.jsdelivr.net/npm/dash-shaka-playback@3.5.0/dist/dash-shaka-playback.min.js",
-            "https://cdn.jsdelivr.net/npm/@clappr/hlsjs-playback@1.8.3/dist/hlsjs-playback.min.js"
-        ], function () {
-            if (/\.(mp4|m3u8|ytb)/i.test(ito)) { setPLAYER(ito); }
-            else if (/(\.html|\/activar)$/i.test(ito)) { setIFRAME(ito); }
-        });
-    }    
+    try {
+        ito = location.href.split('#')[1] || 'menu';
+        if (/(menu)/i.test(ito)) {
+            KScripts(["https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js"], function () { setMENU(); });
+        } else {
+            KScripts([
+                "https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js",
+                "https://cdn.jsdelivr.net/npm/clappr@0.3.13/dist/clappr.min.js",
+                "https://cdn.jsdelivr.net/npm/dash-shaka-playback@3.5.0/dist/dash-shaka-playback.min.js",
+                "https://cdn.jsdelivr.net/npm/@clappr/hlsjs-playback@1.8.3/dist/hlsjs-playback.min.js"
+            ], function () {
+                if (/\.(mp4|m3u8|ytb)/i.test(ito)) { setPLAYER(ito); }
+                else if (/(\.html|\/activar)$/i.test(ito)) { setIFRAME(ito); }
+            });
+        }
+    } catch (erx) { console.error('Error: ' + erx); }
     return false;
 }
 
@@ -63,7 +64,7 @@ function setMENU() {
         data.forEach(el => {
             if (el.url) {
                 itc++;
-                $('<div class="knl0" id="'+itc+'" tipo="' + el.tipo + '" url="' + el.url + '" tabindex="0">' +
+                $('<div class="knl0" id="' + itc + '" tipo="' + el.tipo + '" url="' + el.url + '" tabindex="0">' +
                     '<input class="knl1" type="image" src="' + el.logo + '" onerror="this.onerror=null;">' +
                     '<input class="knl2" type="button" value="' + el.nombre + '">' +
                     '</div>').appendTo('#xplayer');
@@ -115,7 +116,7 @@ function setMENU() {
         $('.knl0').css({
             background: 'rgba(0,0,0,0.5)', margin: '5px', padding: '10px', paddingBottom: '0px', border: '1px solid silver',
             borderRadius: '5px', userSelect: 'none', pointerEvents: 'auto', display: 'grid'
-        }).on('click',function(){ its = parseInt($(this).attr('id')); selKNL(1); });
+        }).on('click', function () { its = parseInt($(this).attr('id')); selKNL(1); });
         $('.knl1').css({
             background: 'rgba(102, 100, 100, 0.5)', border: '1px solid silver', userSelect: 'none', pointerEvents: 'none',
             borderRadius: '5px', width: '280px', height: '130px'
@@ -152,96 +153,142 @@ function selKNL(opt) {
 
 /*--SET-HORAS----------------------*/
 function getHH() {
-    var now = new Date();
-    var hh = String(now.getHours()).padStart(2, '0');
-    var mm = String(now.getMinutes()).padStart(2, '0');
-    var ss = String(now.getSeconds()).padStart(2, '0');
-    var ho = hh + mm + ss;
-    return ho;
+    try {
+        var now = new Date();
+        var hh = String(now.getHours()).padStart(2, '0');
+        var mm = String(now.getMinutes()).padStart(2, '0');
+        var ss = String(now.getSeconds()).padStart(2, '0');
+        var ho = hh + mm + ss;
+        return ho;
+    } catch (erx) { console.error('Error: ' + erx); }
 }
 
 /*--KNLS-SELECTION------------------*/
 function opKNL() {
-    var d1 = $('#' + its).attr('url');
-    var nh = getHH();
-    console.log(d1);
-    if (/\.(mp4|m3u8|ytb)/i.test(d1)) {
-        location.replace('./index.html?' + nh + '#' + d1);
-    } else if (/(\.html|\/activar)$/i.test(d1)) {
-        location.replace('./index.html?' + nh + '#' + d1);
-    }
+    try {
+        var d1 = $('#' + its).attr('url');
+        var nh = getHH();
+        console.log(d1);
+        if (/\.(mp4|m3u8|ytb)/i.test(d1)) {
+            location.replace('./index.html?' + nh + '#' + d1);
+        } else if (/(\.html|\/activar)$/i.test(d1)) {
+            location.replace('./index.html?' + nh + '#' + d1);
+        }
+    } catch (erx) { console.error('Error: ' + erx); }
     return false;
 }
 
-function setPLAYER(vurl){
-    var timeId, player, nVol;
-    $('body').empty();
-    $('<div id="vspm"></div>').css({
-        position: 'absolute', fontWeight: '600', top: '20px', right: '20px', color: 'lime', zIndex: '9999',
-        userSelect: 'none', pointerEvents: 'none'
-    }).appendTo('body');
-    $('<div id="xplayer"></div>').appendTo('body');
+/*--ANDROID-VERSION-----------------*/
+function getAndroidVersion() {
+    const ua = navigator.userAgent;
+    const match = ua.match(/Android\s([0-9\.]+)/); // Busca "Android X.X"
+    return match ? match[1] : null;
+}
 
-    /*--CHECK-VERSION--------------------------------------------------------------------------------------------------*/
-    var androidVer = getAndroidVersion();
-    var Modermplugins = [];
-    var Extraplugins = {};
-    if (androidVer && androidVer < 5) {//MP4 o HLS nativo
-    } else {//Plugin moderno
-        Modermplugins.push(HlsjsPlayback, DashShakaPlayback);
-        Extraplugins = { html5_dash: DashShakaPlayback, html5_hlsjs: HlsjsPlayback };
-    }
-    /*--CHECK-EXT-----------------------------------------------------------------------------------------------------*/    
-    if (/\.(mp4|m3u8)/i.test(vurl)) {
-        player = new Clappr.Player(
-            {
-                parentId: '#xplayer', width: '100vw', height: '100vh', autoPlay: true, mute: false, source: vurl,
-                plugins: Modermplugins,
-                playbackPlugins: Extraplugins,
-                playback: { playInline: true }
-            }).on(Clappr.Events.PLAYER_PLAY, () => { setEPLAYER('video', true); });
-    } else if (/\.(ytb)/i.test(vurl)) {
-        vurl = vurl.split('.')[0];
-        player = new Clappr.Player(
-            {
-                parentId: '#xplayer', width: '100vw', height: '100vh', autoPlay: true, mute: false, source: vurl,
-                poster: 'https://img.youtube.com/vi/' + vurl + '/hqdefault.jpg',                
-                plugins: [YoutubePlugin],
-                youtube: { autoplay: 1, controls: 0, modestbranding: 1, rel: 0, showinfo: 0, fs: 0, iv_load_policy: 3, mute: 0 },
-                playback: { playInline: true }
-            }).on(Clappr.Events.PLAYER_PLAY, () => { setEPLAYER('iframe', true); });
-    }
-    /*--ACTION-PLAY--------------------------------------------------------------------------------------------------*/
-    function setEPLAYER(FOptc, FPlay) {
-        if (FPlay) {
-            FPlay = false;
-            try { player.core.mediaControl.disable(); } catch (error) { }
-            try { player.setVolume(100); } catch (error) { }
-            const iframe = document.querySelector(FOptc);
-            if (iframe) {
-                iframe.style.position = "absolute";/*-*/iframe.style.objectFit = "fill";
-                iframe.style.width = "100%";/*-*/iframe.style.height = "100%";
-                iframe.style.backgroundColor = 'black';/*-*/iframe.style.overflow = 'hidden';
-                iframe.style.top = "0px";/*-*/iframe.style.left = "0px";
+function setPLAYER(vurl) {
+    try {
+        var timeId, player, nVol;
+        $('body').empty();
+        $('<div id="vspm"></div>').css({
+            position: 'absolute', fontWeight: '600', top: '20px', right: '20px', color: 'lime', zIndex: '9999',
+            userSelect: 'none', pointerEvents: 'none'
+        }).appendTo('body');
+        $('<div id="xplayer"></div>').appendTo('body');
+
+        /*--CHECK-VERSION--------------------------------------------------------------------------------------------------*/
+        var androidVer = getAndroidVersion();
+        var Modermplugins = [];
+        var Extraplugins = {};
+        if (androidVer && androidVer < 5) {//MP4 o HLS nativo
+        } else {//Plugin moderno
+            Modermplugins.push(HlsjsPlayback, DashShakaPlayback);
+            Extraplugins = { html5_dash: DashShakaPlayback, html5_hlsjs: HlsjsPlayback };
+        }
+        /*--CHECK-EXT-----------------------------------------------------------------------------------------------------*/
+        if (/\.(mp4|m3u8)/i.test(vurl)) {
+            player = new Clappr.Player(
+                {
+                    parentId: '#xplayer', width: '100vw', height: '100vh', autoPlay: true, mute: false, source: vurl,
+                    plugins: Modermplugins,
+                    playbackPlugins: Extraplugins,
+                    playback: { playInline: true }
+                }).on(Clappr.Events.PLAYER_PLAY, () => { setEPLAYER('video', true); });
+        } else if (/\.(ytb)/i.test(vurl)) {
+            vurl = vurl.split('.')[0];
+            player = new Clappr.Player(
+                {
+                    parentId: '#xplayer', width: '100vw', height: '100vh', autoPlay: true, mute: false, source: vurl,
+                    poster: 'https://img.youtube.com/vi/' + vurl + '/hqdefault.jpg',
+                    plugins: [YoutubePlugin],
+                    youtube: { autoplay: 1, controls: 0, modestbranding: 1, rel: 0, showinfo: 0, fs: 0, iv_load_policy: 3, mute: 0 },
+                    playback: { playInline: true }
+                }).on(Clappr.Events.PLAYER_PLAY, () => { setEPLAYER('iframe', true); });
+        }
+        /*--ACTION-PLAY--------------------------------------------------------------------------------------------------*/
+        function setEPLAYER(FOptc, FPlay) {
+            if (FPlay) {
+                FPlay = false;
+                try { player.core.mediaControl.disable(); } catch (error) { }
+                try { player.setVolume(100); } catch (error) { }
+                const iframe = document.querySelector(FOptc);
+                if (iframe) {
+                    iframe.style.position = "absolute";/*-*/iframe.style.objectFit = "fill";
+                    iframe.style.width = "100%";/*-*/iframe.style.height = "100%";
+                    iframe.style.backgroundColor = 'black';/*-*/iframe.style.overflow = 'hidden';
+                    iframe.style.top = "0px";/*-*/iframe.style.left = "0px";
+                }
             }
         }
-    }
-    /*--SPAM---------------------------------------------------------------------------------------------------------*/
-    function infspm(texto) {
-        var div = document.getElementById("vspm");
-        div.textContent = texto;
-        clearTimeout(timeId);
-        timeId = setTimeout(function () { div.textContent = ""; }, 2000);
-    }
-    /*--CSS---------------------------------------------------------------------------------------------------------*/
-    $('#xplayer').css({
-        position: 'absolute', margin: '0', padding: '0', userSelect: 'none', pointerEvents: 'none', overflow: 'hidden',
-        width: '100vw', height: '100vh', objectFit: 'fill'
-    });
-    /*--KEYDOWN-----------------------------------------------------------------------------------------------------*/
-    $(document).off('keydown').on('keydown', function (ex) {
-        var kCode = ex.keyCode || window.event;
-        if (ex.code === 'Enter' || kCode === 13) {
+        /*--SPAM---------------------------------------------------------------------------------------------------------*/
+        function infspm(texto) {
+            var div = document.getElementById("vspm");
+            div.textContent = texto;
+            clearTimeout(timeId);
+            timeId = setTimeout(function () { div.textContent = ""; }, 2000);
+        }
+        /*--CSS---------------------------------------------------------------------------------------------------------*/
+        $('#xplayer').css({
+            position: 'absolute', margin: '0', padding: '0', userSelect: 'none', pointerEvents: 'none', overflow: 'hidden',
+            width: '100vw', height: '100vh', objectFit: 'fill'
+        });
+        /*--KEYDOWN-----------------------------------------------------------------------------------------------------*/
+        $(document).off('keydown').on('keydown', function (ex) {
+            var kCode = ex.keyCode || window.event;
+            if (ex.code === 'Enter' || kCode === 13) {
+                if (player.isPlaying()) {
+                    player.pause();
+                    infspm('Pause');
+                } else {
+                    player.play();
+                    infspm('Play');
+                }
+            } else if (ex.code === 'Space' || kCode === 32) {
+                if (player.isPlaying()) {
+                    player.pause();
+                    infspm('Pause');
+                } else {
+                    player.play();
+                    infspm('Play');
+                }
+            } else if (ex.key === 'Backspace' || ex.keyCode === 8) {
+                var nh = getHH();
+                location.replace('./index.html?' + nh);
+            } else if (ex.code === 'ArrowUp' || kCode === 38) {
+                nVol = Math.min(player.getVolume() + 5, 100);
+                player.setVolume(nVol);
+                infspm('Vol.: ' + nVol + '%');
+            } else if (ex.code === 'ArrowDown' || kCode === 40) {
+                nVol = Math.min(player.getVolume() - 5, 100);
+                player.setVolume(nVol);
+                infspm('Vol.: ' + nVol + '%');
+            } else if (ex.code === 'ArrowLeft' || kCode === 37) {
+                player.seek(Math.max(player.getCurrentTime() - 10, 0));
+                infspm('Rep.: -10Seg');
+            } else if (ex.code === 'ArrowRight' || kCode === 39) {
+                player.seek(Math.max(player.getCurrentTime() + 10, 0));
+                infspm('Rep.: +10Seg');
+            }
+        }).off('click').on('click', function () {
             if (player.isPlaying()) {
                 player.pause();
                 infspm('Pause');
@@ -249,45 +296,12 @@ function setPLAYER(vurl){
                 player.play();
                 infspm('Play');
             }
-        } else if (ex.code === 'Space' || kCode === 32) {
-            if (player.isPlaying()) {
-                player.pause();
-                infspm('Pause');
-            } else {
-                player.play();
-                infspm('Play');
-            }
-        } else if (ex.key === 'Backspace' || ex.keyCode === 8) { 
-            var nh = getHH();
-            location.replace('./index.html?'+nh);
-        } else if (ex.code === 'ArrowUp' || kCode === 38) {
-            nVol = Math.min(player.getVolume() + 5, 100);
-            player.setVolume(nVol);
-            infspm('Vol.: ' + nVol + '%');
-        } else if (ex.code === 'ArrowDown' || kCode === 40) {
-            nVol = Math.min(player.getVolume() - 5, 100);
-            player.setVolume(nVol);
-            infspm('Vol.: ' + nVol + '%');
-        } else if (ex.code === 'ArrowLeft' || kCode === 37) {
-            player.seek(Math.max(player.getCurrentTime() - 10, 0));
-            infspm('Rep.: -10Seg');
-        } else if (ex.code === 'ArrowRight' || kCode === 39) {
-            player.seek(Math.max(player.getCurrentTime() + 10, 0));
-            infspm('Rep.: +10Seg');
-        }
-    }).off('click').on('click', function () {
-        if (player.isPlaying()) {
-            player.pause();
-            infspm('Pause');
-        } else {
-            player.play();
-            infspm('Play');
-        }
-    });
+        });
+    } catch (erx) { console.error('Error: ' + erx); }
     return false;
 }
 
-function setIFRAME(zurl){
+function setIFRAME(zurl) {
     $('body').empty();
     $('<iframe ' +
         'id="frmplayer"' +
@@ -308,9 +322,9 @@ function setIFRAME(zurl){
     });
     /*--KEYDOWN-----------------------------------------------------------------------------------------------------*/
     $(document).off('keydown').on('keydown', function (ex) {
-        if (ex.key === 'Backspace' || ex.keyCode === 8) { 
+        if (ex.key === 'Backspace' || ex.keyCode === 8) {
             var nh = getHH();
-            location.replace('./index.html?'+nh);
+            location.replace('./index.html?' + nh);
         }
     }).off('click').on('click', function () { });
     return false;
